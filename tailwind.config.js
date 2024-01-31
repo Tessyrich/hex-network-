e8f60a3a453e48cab0e15aaf7f18f93bc25562be
Tessyrich <olyrich3@gmail.com>
2024-01-31 14:51:07 +0100

    update content

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         deorange: "#eb5d47",
         depurple: "#a933b2",
         degreen: "#a4f9ff",
+        iconbg: "hsla(0,0%,100%,0.1)",
       },
       fontSize: {
         big: "9rem",
